30e5aea81d310e9aa8d4cedf988945c3776093e4
Aryo nurwanto w <aryo1291@gmail.com>
2026-02-25 14:24:31 +0700

    update failed build

--- src/lib/richtext/richtext.ts
+++ src/lib/richtext/richtext.ts
@@ -1,8 +1,6 @@
-import { lexicalToHTML } from '@payloadcms/richtext-lexical'
+// import { lexicalHTML } from '@payloadcms/richtext-lexical'
 
-export async function richTextToHTML(richText: any): Promise<string> {
-  if (!richText) return ''
-  return lexicalToHTML({
-    data: richText,
-  })
-}
+// export async function richTextToHTML(richText: any): Promise<string> {
+//   if (!richText) return ''
+//   return lexicalHTML(richText)
+// }
