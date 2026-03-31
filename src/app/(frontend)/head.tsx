3aa023cb173521554269aafdd4ae74a6cf706c1c
Aryo nurwanto w <aryo1291@gmail.com>
2026-03-31 10:55:34 +0700

    update icon tab and title tab

--- src/app/(frontend)/head.tsx
+++ src/app/(frontend)/head.tsx
@@ -1,6 +1,7 @@
 export default function Head() {
   return (
     <>
+      <title>Mobis</title>
       <link rel="icon" href="./mobis/img/favicon.ico" sizes="32x32" />
       <link rel="icon" href="./mobis/img/favicon.svg" type="image/svg+xml" />
       <link rel="stylesheet" href="./mobis/style/style.css" />
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -15,6 +15,7 @@ import { getServerSideURL } from '@/utilities/getURL'
 
 const generateTitle: GenerateTitle<Post | Page> = ({ doc }) => {
   return doc?.title ? `${doc.title} | Payload Website Template` : 'Payload Website Template'
+  // return `Mobis`
 }
 
 const generateURL: GenerateURL<Post | Page> = ({ doc }) => {
