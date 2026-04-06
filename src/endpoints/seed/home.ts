4d8b12e25a55c56adca246b6d897dda66ef6509b
Aryo nurwanto w <aryo1291@gmail.com>
2026-04-06 08:41:54 +0700

    update meta

--- src/endpoints/seed/home.ts
+++ src/endpoints/seed/home.ts
@@ -47,7 +47,7 @@ export const home: (args: HomeArgs) => RequiredDataFromCollectionSlug<'pages'> =
                   format: 0,
                   mode: 'normal',
                   style: '',
-                  text: 'Payload Website Template',
+                  text: 'Mobis Car rent website',
                   version: 1,
                 },
               ],
@@ -666,9 +666,9 @@ export const home: (args: HomeArgs) => RequiredDataFromCollectionSlug<'pages'> =
       },
     ],
     meta: {
-      description: 'An open-source website built with Payload and Next.js.',
+      description: 'WeBSITE for Mobis, a Car rent company.',
       image: heroImage.id,
-      title: 'Payload Website Template',
+      title: ' Template',
     },
     title: 'Home',
   }
--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -3,14 +3,14 @@ import { getServerSideURL } from './getURL'
 
 const defaultOpenGraph: Metadata['openGraph'] = {
   type: 'website',
-  description: 'An open-source website built with Payload and Next.js.',
+  description: 'WeBSITE for Mobis, a Car rent company.',
   images: [
     {
       url: `${getServerSideURL()}/website-template-OG.webp`,
     },
   ],
-  siteName: 'Payload Website Template',
-  title: 'Payload Website Template',
+  siteName: 'Mobis Website',
+  title: 'Mobis Website',
 }
 
 export const mergeOpenGraph = (og?: Metadata['openGraph']): Metadata['openGraph'] => {
